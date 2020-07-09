8159c99c894b97584f6910a72dfd69d3a4e50fcc
probe2k <kyash054@gmail.com>
2020-07-09 20:47:10 +0530

    key access | key pass to json.decode

--- lib/getter.dart
+++ lib/getter.dart
@@ -2,5 +2,5 @@ import 'dart:async' show Future;
 import 'dart:convert';
 
 Future<String> loadAsset() async {
-  return await json.decode('key.json').cast<Map<String, dynamic>>();
+  return await json.decode('key.json').cast<Map<String, dynamic>>()["api_key"];
 }
