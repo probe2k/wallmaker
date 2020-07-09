8f468a0cb3862f7704b89ae38faeeaaacad836ee
probe2k <kyash054@gmail.com>
2020-07-09 20:43:47 +0530

    Unused imports cleared

--- lib/getter.dart
+++ lib/getter.dart
@@ -1,6 +1,5 @@
 import 'dart:async' show Future;
 import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
 
 Future<String> loadAsset() async {
   return await json.decode('key.json').cast<Map<String, dynamic>>();
